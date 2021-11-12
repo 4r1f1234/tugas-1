581504a509b4334efa625006f90ca775e8835a29
4r1f1234 <94139369+4r1f1234@users.noreply.github.com>
2021-11-12 08:27:31 +0700

    test cryptotab

---
+++ cfg.php
@@ -0,0 +1,26 @@
+<?php
+
+/* data mining */
+
+$sign = "b54JQxd2kCTqY/fEVH26fo4thynDlpws36lX0YTeRH3FUPMYDRHg1XmCAq+8AzKInWTvEgPiaou4xS+G";
+$auth = "Bearer gAAAAABhjVkBWQt84bstXkzVPPqmy412p6G53ggBzTx8_K11qdwi8zKN-O736nRSXkutTEs5XcIOIo7tg4W1Fv-yDbTCuEcHnQ==";
+
+/* data email */
+
+$sign1 = "ijLXGeittO1o7K+Tw7Pc1+MXvuHYhRdkgQlgciiVGSOKeC74Rsrf6V8frITJDB8pr2r192AAg/zBndkD";
+$auth1 = "Bearer gAAAAABhjVzzXHU8vFBOnRS38Bd6--OgZ4_G_R198Rc0bcQbH_YWTyzuHuhGGxvklCfMiqqWAqjec0xtuBpUhB4Yk4wIJqScrw==";
+
+
+/* cookie */
+
+$user_agent = "Mozilla/5.0 (Linux; Android 8.1.0; ASUS_X00TD) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.114 Mobile Safari/537.36";
+$ct_ts = "1636654323";
+$token = "gvoMdV6iogXDfdyGLJjCeFjupou4BvRnLnYoxLmm11plq4UfFoClM25NeUpSWFHl";
+$session = "vljpf3mi62q20jgqkhwy5o3qf7js6ufe";
+$bid = "945c8e0c-42ad-11ec-a11f-02420a030218";
+$ct_v = "6.0.19";
+$ct_av = "2.0.0.258";
+
+
+
+?>
